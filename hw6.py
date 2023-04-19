ae0da170f5440f39d0b67e9eb9d915d5d2db215e
songhj8304 <127584828+songhj8304@users.noreply.github.com>
2023-04-19 09:22:33 +0900

    Add files via upload

---
+++ hw6.py
@@ -0,0 +1,13 @@
+for j in range(1,10):
+    for i in range(2,6):
+        print(f'{i}x{j}={i*j:2d}','\t',end='')
+    print(' ')
+
+
+print()
+
+
+for j in range(1,10):
+    for i in range(6,10):
+        print(f'{i}x{j}={i*j:2d}','\t',end='')
+    print(' ')
